9d52e313cb73a31298c8a8be845f54637017ddcc
LNicoF <65499919+LNicoF@users.noreply.github.com>
2024-10-06 21:38:15 -0300

    pending change

--- src/object_set/object_set_itf.h
+++ src/object_set/object_set_itf.h
@@ -12,7 +12,7 @@ SET_IID( IObjectSet, "f1c47d06-7560-4272-a819-a77f52b22f74" ) ;
 IObjectSet* createObjectSet() ;
 
 template< typename Itf >
-inline Itf* get( IObjectSet* objectSet ) {
+inline Itf* get( IObjectSet* objectSet ) { // Maybe factory optional argument to add parameters to factories
     IObject* result = objectSet->get( Iid< Itf >::getIid(), Factory< Itf >() ) ;
     return ( Itf* )result ;
 }
